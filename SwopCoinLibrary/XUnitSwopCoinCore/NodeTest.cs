a123bdae300d5fa4552d9386b67b2ff68848c135
Brandon Hilde <bjames.12299@gmail.com>
2020-07-19 22:13:41 -0700

    working on improving unit tests

--- SwopCoinLibrary/XUnitSwopCoinCore/NodeTest.cs
+++ SwopCoinLibrary/XUnitSwopCoinCore/NodeTest.cs
@@ -11,13 +11,14 @@ using Xunit.Sdk;
 
 namespace XUnitSwopCoinCore
 {
+    [Collection("Sequential")]
     public class NodeTest
     {
-        BtcNodeCreate create = new BtcNodeCreate();
-
         [Fact]
         public void SetupNetwork()
         {
+            BtcNodeCreate create = new BtcNodeCreate();
+
             BtcNodeCreate.ActionStatus res = create.SetUpBuilder(NodeDownloadData.Bitcoin.v0_18_0, Network.RegTest);
 
             Assert.True(res.Success);
@@ -26,9 +27,14 @@ namespace XUnitSwopCoinCore
         [Fact] 
         public void CreateNodes()
         {
-            BtcNodeCreate.ActionStatus res = create.CreateNodeSet(3);
+            BtcNodeCreate create = new BtcNodeCreate();
+
+            BtcNodeCreate.ActionStatus res = create.SetUpBuilder(NodeDownloadData.Bitcoin.v0_18_0, Network.RegTest);
+
+            BtcNodeCreate.ActionStatus resN = create.CreateNodeSet(3);
 
             Assert.True(res.Success);
+            Assert.True(resN.Success);
         }
 
         [Theory]
@@ -37,6 +43,15 @@ namespace XUnitSwopCoinCore
         [InlineData("bob")]
         public void NameNodes(string name)
         {
+            BtcNodeCreate create = new BtcNodeCreate();
+
+            BtcNodeCreate.ActionStatus res = create.SetUpBuilder(NodeDownloadData.Bitcoin.v0_18_0, Network.RegTest);
+
+            BtcNodeCreate.ActionStatus resN = create.CreateNodeSet(3);
+
+            Assert.True(res.Success);
+            Assert.True(resN.Success);
+
             create.NameNextNode(name);
 
             Assert.NotNull(create.GetByName(name));
@@ -45,6 +60,8 @@ namespace XUnitSwopCoinCore
         [Fact]
         public void NetworkCreate()
         {
+            BtcNodeCreate create = new BtcNodeCreate();
+
             List<uint256> list = create.StartNetwork();
 
             if (list != null)
@@ -62,6 +79,8 @@ namespace XUnitSwopCoinCore
         [Fact]
         public void CreateAddresses()
         {
+            BtcNodeCreate create = new BtcNodeCreate();
+
             BtcNodeCreate.ActionStatus res = create.AddAddresses();
 
             Assert.True(res.Success);
@@ -70,28 +89,126 @@ namespace XUnitSwopCoinCore
         [Fact]
         public void SendBitcoin()
         {
-            BtcNodeCreate.ActionStatus res = create.SendBitcoin(
+            BtcNodeCreate create = new BtcNodeCreate();
+
+            BtcNodeCreate.ActionStatus res = create.SetUpBuilder(NodeDownloadData.Bitcoin.v0_18_0, Network.RegTest);
+
+            BtcNodeCreate.ActionStatus resN = create.CreateNodeSet(3);
+
+            Assert.True(res.Success);
+            Assert.True(resN.Success);
+
+            create.NameNextNode("miner");
+            create.NameNextNode("alice");
+
+            Assert.NotNull(create.GetByName("miner"));
+            Assert.NotNull(create.GetByName("alice"));
+
+            List<uint256> list = create.StartNetwork();
+
+            if (list != null)
+            {
+                if (list.Count > 1)
+                {
+                    for (int i = 1; i < list.Count; i++)
+                    {
+                        Assert.True(list[i] != list[i - 1]);
+                    }
+                }
+            }
+
+            BtcNodeCreate.ActionStatus resM = create.MineNetwork(create.GetByName("miner"), 1);
+
+            Assert.True(resM.Success);
+
+            BtcNodeCreate.ActionStatus resS = create.SendBitcoin(
                 create.GetByName("miner"), 
                 Money.Coins(20m), 
                 create.GetByName("alice"));
 
-            Assert.True(res.Success);
+            Assert.True(resS.Success);
         }
 
         [Fact]
         public void MineBitcoin()
         {
-            BtcNodeCreate.ActionStatus res = create.MineNetwork(create.GetByName("miner"), 1);
+            BtcNodeCreate create = new BtcNodeCreate();
+
+            BtcNodeCreate.ActionStatus res = create.SetUpBuilder(NodeDownloadData.Bitcoin.v0_18_0, Network.RegTest);
+
+            BtcNodeCreate.ActionStatus resN = create.CreateNodeSet(3);
 
             Assert.True(res.Success);
+            Assert.True(resN.Success);
+
+            create.NameNextNode("miner");
+            create.NameNextNode("alice");
+
+            Assert.NotNull(create.GetByName("miner"));
+            Assert.NotNull(create.GetByName("alice"));
+
+            List<uint256> list = create.StartNetwork();
+
+            if (list != null)
+            {
+                if (list.Count > 1)
+                {
+                    for (int i = 1; i < list.Count; i++)
+                    {
+                        Assert.True(list[i] != list[i - 1]);
+                    }
+                }
+            }
+
+            BtcNodeCreate.ActionStatus resM = create.MineNetwork(create.GetByName("miner"), 1);
+
+            Assert.True(resM.Success);
         }
 
         [Fact]
         public void SyncNode()
         {
-            BtcNodeCreate.ActionStatus res = create.SyncNodes(create.GetByName("alice"), create.GetByName("miner"));
+            BtcNodeCreate create = new BtcNodeCreate();
+
+            BtcNodeCreate.ActionStatus res = create.SetUpBuilder(NodeDownloadData.Bitcoin.v0_18_0, Network.RegTest);
+
+            BtcNodeCreate.ActionStatus resN = create.CreateNodeSet(3);
 
             Assert.True(res.Success);
+            Assert.True(resN.Success);
+
+            create.NameNextNode("miner");
+            create.NameNextNode("alice");
+
+            Assert.NotNull(create.GetByName("miner"));
+            Assert.NotNull(create.GetByName("alice"));
+
+            List<uint256> list = create.StartNetwork();
+
+            if (list != null)
+            {
+                if (list.Count > 1)
+                {
+                    for (int i = 1; i < list.Count; i++)
+                    {
+                        Assert.True(list[i] != list[i - 1]);
+                    }
+                }
+            }
+
+            BtcNodeCreate.ActionStatus resM = create.MineNetwork(create.GetByName("miner"), 1);
+
+            Assert.True(resM.Success);
+
+            BtcNodeCreate.ActionStatus resSync = create.SyncNodes(create.GetByName("alice"), create.GetByName("miner"));
+
+            Assert.True(resSync.Success);
         }
+    }
+
+    [Collection("Sequential")]
+    public class SecondTestSet
+    {
+
     }
 }
